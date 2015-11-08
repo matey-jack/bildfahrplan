db39362c2e1374675769565a378db76e4bbd4155
Robert J. Will <matey.jack@gmail.com>
2015-11-08 23:10:07 +0100

    collect all station suggestion for given station (too many results, just commit this for documentation)

--- src/main/scala/fahrplan/crawler/FetchStationIds.scala
+++ src/main/scala/fahrplan/crawler/FetchStationIds.scala
@@ -1,17 +1,21 @@
 package fahrplan.crawler
 
-import scala.collection.JavaConverters._
-
 import java.nio.charset.Charset
-import java.nio.file.{Paths, Files}
+import java.nio.file.{Files, Paths}
 
 import de.schildbach.pte.BahnProvider
-
-import scala.collection.{mutable, concurrent}
+import de.schildbach.pte.dto.{Location, LocationType}
 import org.json4s.jackson.Serialization
 
-case class HafasStation(id : String, name : String, place : String)
+import scala.collection.JavaConverters._
 
+case class HafasStation(id: String, name: String, place: String)
+
+object HafasStation {
+  def fromLocation(l: Location): HafasStation = {
+    HafasStation(l.id, l.name, l.place)
+  }
+}
 
 object FetchStationIds {
   val provider = new BahnProvider
@@ -20,12 +24,14 @@ object FetchStationIds {
   def main(args: Array[String]): Unit = {
 
     val scrapedStationLines = Files.readAllLines(Paths.get("raw_stations.json"), Charset.forName("UTF-8")).asScala
-    val s = scrapedStationLines.map(Serialization.read[ScrapedStation]).head
-    println(provider.suggestLocations(s.name).getLocations)
-    // val stationsById = concurrent.TrieMap[String, HafasStation]()
-    // provider.suggestLocations().suggestedLocations
+    val grouped = (scrapedStationLines take 20).par.flatMap { line =>
+      val n: String = Serialization.read[ScrapedStation](line).name
+      provider.suggestLocations(n).getLocations.asScala.
+        filter(_.`type` == LocationType.STATION).toSet
+    }.map(HafasStation.fromLocation)
 
-    // filter(_.type == LocationType.STATION)
+
+    print(grouped)
   }
 }
 
